6fe5596faa8e63c873585c263a00878bab772645
prushton2 <peterrushton52@gmail.com>
2019-01-10 18:27:07 -0500

    Finishing the basic math function

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ def dprint(ipt):   #ipt = input, use as a tuple
     if (debug):
         print('DEBUG:', ipt)
 
+def factorial(x):
+    y = 1
+    while(x > 0):
+        y = y*x
+        x = x-1
+    return y
 
 def math(ipt):   #input a string in the format of '10 + 5'
     dprint(ipt)
@@ -27,11 +33,12 @@ def math(ipt):   #input a string in the format of '10 + 5'
         return problem2[0] * problem2[1]
     elif operator == '/':
         return problem2[0] / problem2[1]
+    elif operator == '!':
+        return factorial(problem2[0])
     else:
         return("Invalid Operator")
 
 
-print(math('5 + 10'))
+print(math('54.1 + .1'))
 
-
-
+print(factorial(5))
